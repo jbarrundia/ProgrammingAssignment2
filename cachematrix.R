7df203463ab0fd6f0546c159c2d23a9be3971f70
Jose Barrundia <jbarrundia@hotmail.com>
2020-07-26 18:35:44 -0600

    Se definieron ya las dos funciones.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  gl <- NULL
+  set <- function(y) {
+    x <<- y
+    gl <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) gl <<- inv
+  getinv <- function() gl
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)  
 
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
